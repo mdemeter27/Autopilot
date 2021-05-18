75bb8752da3f42d1cf0d4620313caef66d52e119
Matthew Demeter <mdemeter27@gmai.com>
2021-05-18 16:41:34 -0500

    Got receiver to work with sbus. It can read all 16 channels, but for some reason reading the sbus while reading the LSM sensor causes the sbus to stop working. May need to modify/change sbus reading code

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,6 +1,7 @@
 
+
 bool serialConnected = true;//MUST TURN THIS TO FALSE FOR FLIGHT
-int mode = 2;
+int mode = 0;
 // 0 is unArmed
 // 1 is manual control
 // 2 is stabilized mode
@@ -10,29 +11,38 @@ int mode = 2;
 
 void setup() {
   if (serialConnected) {
-    Serial.begin(9600);
+    Serial.begin(115200);
     while (!Serial);
   }
   lsmSetup();
+  receiverSetup();
 }
 
 void loop() {
+
   while (mode == 0) {
+    receiverLoop();
+    setMode();
 
   }
   while (mode == 1) {
-    //Do something
+    receiverLoop();
+    setMode();
+
   }
 
-  while (mode == 2) {
+  while (mode == 2) {//FIXME Receiver will not work while reading lsm
+    receiverLoop();
+    setMode();
+
     stabilize();
   }
 }
 
-void error(String s){
- // mode = 1;
+void error(String s) {
+  // mode = 1;
   //if(serialConnected){
   //  Serial.println(s + " going to manual control");
   //}
-  
+
 }
